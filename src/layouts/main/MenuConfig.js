ffed159e7e4eaf44941aa8647d3ab3b61722f019
OliveBounty <oliveodobounty@gmail.com>
2021-10-16 10:00:52 +0900

    Initial Project

---
+++ src/layouts/main/MenuConfig.js
@@ -0,0 +1,48 @@
+import { Icon } from '@iconify/react';
+import homeFill from '@iconify/icons-eva/home-fill';
+import fileFill from '@iconify/icons-eva/file-fill';
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+
+// ----------------------------------------------------------------------
+
+const ICON_SIZE = {
+  width: 22,
+  height: 22
+};
+
+const menuConfig = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <Icon icon={homeFill} {...ICON_SIZE} />
+  },
+  {
+    title: 'Host',
+    path: '/',
+    icon: <Icon icon={homeFill} {...ICON_SIZE} />
+  },
+  {
+    title: 'Rentals',
+    path: '/',
+    icon: <Icon icon={homeFill} {...ICON_SIZE} />
+  },
+  {
+    title: 'Learn More',
+    path: '/',
+    icon: <Icon icon={homeFill} {...ICON_SIZE} />
+  },
+  {
+    title: 'Messages',
+    path: '/',
+    icon: <Icon icon={homeFill} {...ICON_SIZE} />
+  },
+  {
+    title: 'Account',
+    path: '/',
+    icon: <Icon icon={homeFill} {...ICON_SIZE} />
+  }
+  // { title: 'Dashboard', path: PATH_DASHBOARD.root, icon: <Icon icon={fileFill} {...ICON_SIZE} /> }
+];
+
+export default menuConfig;
